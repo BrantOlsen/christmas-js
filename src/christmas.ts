8fd71b673172fe0bb2f45975d896192406dca436
Brant Olsen <brant.olsen@webcemeteries.com>
2019-12-15 21:55:38 -0500

    Moving div.

--- src/christmas.ts
+++ src/christmas.ts
@@ -1,18 +1,33 @@
 class Snowflake {
-  x: number;
-  y: number;
-  canvas: Element;
+  x: number = 0;
+  y: number = 0;
+  canvas: HTMLDivElement;
 
   constructor() {
     this.canvas = document.createElement("div");
+    document.body.appendChild(this.canvas);
     this.canvas.setAttribute('width', '100px');
     this.canvas.setAttribute('height', '100px');
-    this.canvas.setAttribute('style', 'background-color: blue;');
+    this.canvas.setAttribute('style', 'background-color: blue; width: 100px; height: 100px; position: fixed; top: 0; left: 0;');
+    
   }
 
   draw(): void {
-
+    this.x += 10;
+    this.y += 10;
+    if (this.x > window.innerWidth) {
+      this.x = 0;
+    }
+    if (this.y > window.innerHeight) {
+      this.y = 0;
+    }
+    
+    this.canvas.style.left = this.x + "px";
+    this.canvas.style.top = this.y + "px";
   }
 }
 
 let t = new Snowflake();
+setInterval(() => {
+  t.draw();
+}, 100);
