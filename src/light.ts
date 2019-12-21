f081c2e2cb61c2359c85a23a013b9ef9d5529344
Brant Olsen <brant.olsen@webcemeteries.com>
2019-12-21 18:53:21 -0500

    Make a bulb.

--- src/light.ts
+++ src/light.ts
@@ -14,21 +14,24 @@ class Light {
     let ctx = this._canvas.getContext('2d');
     let endpoint = {x: this.width / 2, y: 0};
 
+
+    
     // Right Side
-    let controlPoint1 = {x: 0, y: this.height / 2};
-    let controlPoint2 = {x: this.height, y: this.height / 2};
+    let rstart = {x: this.height - this.width, y: this.height - 1};
+    let rcp1 = {x: 0, y: this.height / 3};
+    let rcp2 = {x: rstart.x, y: this.height / 2};
     ctx.beginPath();
-    ctx.moveTo(this.height - this.width, this.height - 1);
-    ctx.bezierCurveTo(controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y, endpoint.x, endpoint.y);
+    ctx.moveTo(rstart.x, rstart.y);
+    ctx.bezierCurveTo(rcp1.x, rcp1.y, rcp2.x, rcp2.y, endpoint.x, endpoint.y);
     ctx.strokeStyle = "green";
     ctx.stroke();
     // Left Side
     let lstart = {x: this.width, y: this.height - 1};
-    let lcp1 = {x: 0, y: this.height / 2};
-    let lcp2 = {x: this.height, y: this.height / 2};
+    let lcp1 = {x: lstart.x, y: rcp1.y};
+    let lcp2 = {x: this.height, y: rcp2.y};
     ctx.beginPath();
     ctx.moveTo(lstart.x, lstart.y);
-    ctx.bezierCurveTo(lcp1.x, lcp1.y, lcp2.x, lcp2.y, endpoint.x, endpoint.y);
+    ctx.bezierCurveTo(lcp2.x, lcp2.y, lcp1.x, lcp1.y, endpoint.x, endpoint.y);
     ctx.strokeStyle = "blue";
     ctx.stroke();
     // Bottom Line
