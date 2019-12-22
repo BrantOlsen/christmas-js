79b2fce11fcca1f944313a5ddba835d5bf54892e
Brant Olsen <brant.olsen@webcemeteries.com>
2019-12-22 13:21:09 -0500

    Break the snow-storm out.

--- src/christmas.ts
+++ src/christmas.ts
@@ -1,49 +1,7 @@
-/// <reference path="./snowflake.ts" />
+/// <reference path="./snow-storm.ts" />
 /// <reference path="./light-string.ts" />
 
-class Storm {
-  snowflakes: Snowflake[] = [];
-  flakesAtATime: number = 10;
-  flakeMinSize = 15;
-  flakeMaxSize = 20;
-  flakeMinPoints = 5;
-  flakeMaxPoints = 6;
-  flakeSpeed = 2;
-  flakeDrift = 5;
-  createSnowInterval: number;
-
-  constructor() {
-    setInterval(() => {
-      this.snowflakes.forEach(flake => {
-        flake.speed = this.flakeSpeed; 
-        flake.drift = this.flakeDrift;
-        flake.draw();
-      });
-    }, 50); // 20 FPS
-  }
-
-  startSnow() {
-    this.createSnowInterval = setInterval(() => {
-      let distanceBetweenFlakes = window.innerWidth / this.flakesAtATime;
-      let i = distanceBetweenFlakes;
-      while (i < window.innerWidth) {
-        let flakePoints = Math.floor((this.flakeMaxPoints - this.flakeMinPoints + 1) * Math.random() + this.flakeMinPoints);
-        let flakeSize = Math.floor((this.flakeMaxSize - this.flakeMinSize + 1) * Math.random() + this.flakeMinSize);
-        let flake = new Snowflake(0, i, flakePoints, flakeSize);
-        this.snowflakes.push(flake);
-        
-        i += distanceBetweenFlakes;
-      }
-
-      // Stop after creating enough snow for the whole window.
-      if (this.snowflakes.length > ((this.flakesAtATime + 1) * (window.innerHeight / this.snowflakes[0].size))) {
-        clearInterval(this.createSnowInterval);
-      }
-    }, 1000);
-  }
-}
-
-let t = new Storm();
+let t = new SnowStorm();
 t.startSnow();
 
 var l = new LightString(LightLocation.Bottom);
---
+++ src/snow-storm.ts
@@ -0,0 +1,43 @@
+/// <reference path="./snowflake.ts" />
+
+class SnowStorm {
+  snowflakes: Snowflake[] = [];
+  flakesAtATime: number = 10;
+  flakeMinSize = 10;
+  flakeMaxSize = 15;
+  flakeMinPoints = 5;
+  flakeMaxPoints = 6;
+  flakeSpeed = 2;
+  flakeDrift = 5;
+  createSnowInterval: number;
+
+  constructor() {
+    setInterval(() => {
+      this.snowflakes.forEach(flake => {
+        flake.speed = this.flakeSpeed;
+        flake.drift = this.flakeDrift;
+        flake.draw();
+      });
+    }, 50); // 20 FPS
+  }
+
+  startSnow() {
+    this.createSnowInterval = setInterval(() => {
+      let distanceBetweenFlakes = window.innerWidth / this.flakesAtATime;
+      let i = distanceBetweenFlakes;
+      while (i < window.innerWidth) {
+        let flakePoints = Math.floor((this.flakeMaxPoints - this.flakeMinPoints + 1) * Math.random() + this.flakeMinPoints);
+        let flakeSize = Math.floor((this.flakeMaxSize - this.flakeMinSize + 1) * Math.random() + this.flakeMinSize);
+        let flake = new Snowflake(0, i, flakePoints, flakeSize);
+        this.snowflakes.push(flake);
+
+        i += distanceBetweenFlakes;
+      }
+
+      // Stop after creating enough snow for the whole window.
+      if (this.snowflakes.length > ((this.flakesAtATime + 1) * (window.innerHeight / this.snowflakes[0].size))) {
+        clearInterval(this.createSnowInterval);
+      }
+    }, 1000);
+  }
+}
